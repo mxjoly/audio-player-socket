2ad0ad1cb4d1ff7b0f730e1ef933d85a9256fae8
Maxime Joly <maxime.joly2808@gmail.com>
2020-11-06 15:03:04 +0100

    Set the host environment variable

--- config/env.js
+++ config/env.js
@@ -39,7 +39,7 @@ module.exports = () => {
   const raw = {
     PORT: process.env.PORT || 8500,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    HOST: `https://music-player-sync.herokuapp.com`,
+    HOST: process.env.HOST || `http://localhost:${process.env.PORT || 8500}`,
   };
 
   // Stringify all values so we can feed into Webpack DefinePlugin
